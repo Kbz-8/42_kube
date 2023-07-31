1b2a3a55c29e98511a81a09e236c36fedc15887d
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-07-31 18:21:51 +0200

    caca

--- includes/renderer.h
+++ includes/renderer.h
@@ -6,12 +6,20 @@
 /*   By: maldavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 17:39:53 by maldavid          #+#    #+#             */
-/*   Updated: 2023/07/31 17:53:14 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/07/31 18:02:04 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RENDERER_H
 # define RENDERER_H
+
+typedef struct	s_textures_files
+{
+	const char	*north;
+	const char	*south;
+	const char	*west;
+	const char	*est;
+}	t_textures_files;
 
 typedef struct	s_textures
 {
@@ -41,7 +49,7 @@ typedef struct	s_renderer
 	t_world		*world;
 }	t_renderer;
 
-t_renderer	*init_renderer(t_textures *textures, t_world *world);
+t_renderer	*init_renderer(t_textures_files *textures, t_world *world);
 void		render(t_renderer *renderer);
 
 #endif
--- srcs/renderer/init.c
+++ srcs/renderer/init.c
@@ -6,17 +6,17 @@
 /*   By: maldavid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 17:39:45 by maldavid          #+#    #+#             */
-/*   Updated: 2023/07/31 17:56:34 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/07/31 18:09:01 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <memory.h>
 
-t_renderer	*init_renderer(t_textures *textures, t_world *world)
+t_renderer	*init_renderer(t_textures_files *textures, t_world *world)
 {
 	t_renderer	*renderer;
 
 	renderer = alloc(sizeof(t_renderer));
-	renderer->textures
+	renderer->world = world;
 	return (renderer);
 }
