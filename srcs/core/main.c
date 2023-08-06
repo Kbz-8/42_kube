54f3e70daad1e3be943f2756cac8d27a288a3a25
Kbz-8 <kbz_8.dev@akel-engine.com>
2023-08-06 20:19:28 +0200

    caca prout mdr rammus

--- srcs/core/main.c
+++ srcs/core/main.c
@@ -6,7 +6,7 @@
 /*   By: vvaas <vvaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 16:50:34 by maldavid          #+#    #+#             */
-/*   Updated: 2023/08/01 03:19:40 by maldavid         ###   ########.fr       */
+/*   Updated: 2023/08/06 20:18:53 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int ac, char **av)
 {
 	t_application	app;
 
+	ft_set_internal_malloc(alloc);
+	ft_set_internal_free(dealloc);
 	parse(ac, av);
 	init_application(&app);
 	run(&app);
